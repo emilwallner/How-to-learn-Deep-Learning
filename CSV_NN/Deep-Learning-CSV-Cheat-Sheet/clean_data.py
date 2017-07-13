dbf0d749c1cea5fa6ed1f6c00bd5f1436474bd8f
Emil Wallner <w@llner.co>
2017-07-13 09:42:36 +0200

    Clean data 🛀"

---
+++ CSV_NN/Deep-Learning-CSV-Cheat-Sheet/clean_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+def clean_data(data):
+    data["Age"].fillna(data["Age"].mean(), inplace=True)
+    data.drop(data.columns[[0, 3, 8, 10]], axis=1, inplace=True)
+    data.replace(['male', 'female'], [1, 2], inplace=True)
+    data.replace(['S', 'C', 'Q', np.nan], [1, 2, 3, 0], inplace=True)
+    return data
