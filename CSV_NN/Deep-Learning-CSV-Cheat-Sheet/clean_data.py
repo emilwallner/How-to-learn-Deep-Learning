4a536ee9013bb6508c1bde3cfa571c7aff00714a
Emil Wallner <w@llner.co>
2017-07-13 20:51:53 +0200

    Neural net design 🍥

--- CSV_NN/Deep-Learning-CSV-Cheat-Sheet/clean_data.py
+++ CSV_NN/Deep-Learning-CSV-Cheat-Sheet/clean_data.py
@@ -4,6 +4,5 @@ import numpy as np
 def clean_data(data):
     data["Age"].fillna(data["Age"].mean(), inplace=True)
     data.drop(data.columns[[0, 2, 7, 9]], axis=1, inplace=True)
-    data.replace(['male', 'female'], [1, 2], inplace=True)
-    data.replace(['S', 'C', 'Q', np.nan], [1, 2, 3, 0], inplace=True)
-    return data
+    data = pd.get_dummies(data, columns=['Sex' , 'Embarked', 'Pclass'], drop_first=True)
+    return np.array(data, dtype=np.float32)
