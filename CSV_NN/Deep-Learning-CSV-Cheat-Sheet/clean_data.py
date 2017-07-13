a3aac6cc17185414a29f9f96dd75c102f7fb4188
Emil Wallner <w@llner.co>
2017-07-13 14:30:19 +0200

    Titanic running 🚢

--- CSV_NN/Deep-Learning-CSV-Cheat-Sheet/clean_data.py
+++ CSV_NN/Deep-Learning-CSV-Cheat-Sheet/clean_data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def clean_data(data):
     data["Age"].fillna(data["Age"].mean(), inplace=True)
-    data.drop(data.columns[[0, 3, 8, 10]], axis=1, inplace=True)
+    data.drop(data.columns[[0, 2, 7, 9]], axis=1, inplace=True)
     data.replace(['male', 'female'], [1, 2], inplace=True)
     data.replace(['S', 'C', 'Q', np.nan], [1, 2, 3, 0], inplace=True)
     return data
