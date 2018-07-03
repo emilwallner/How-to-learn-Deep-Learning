2c2de9dc2f0c6b4cd0933acc9dddd6a37fa0838f
Emil Wallner <w@llner.co>
2018-07-03 08:51:09 +0200

    Clean up

--- NN/Cheat-Sheet/clean_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-def clean_data(data):
-    data["Age"].fillna(data["Age"].mean(), inplace=True)
-    data.drop(data.columns[[0, 2, 7, 9]], axis=1, inplace=True)
-    data = pd.get_dummies(data, columns=['Sex' , 'Embarked', 'Pclass'], drop_first=True)
-    return np.array(data, dtype=np.float32)
